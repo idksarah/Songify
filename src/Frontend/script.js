220e45d1752b6fbec30bee11ed68dcf70ca5082f
idksarah <sarah.p.ngai@gmail.com>
2024-08-16 20:54:15 -0700

    Basic styling

--- src/Frontend/script.js
+++ src/Frontend/script.js
@@ -1,4 +1,4 @@
-async function getSong(lyric){
+/*async function getSong(lyric){
     let url = 'http://127.0.0.1:8000/api/find-songs';
     let tags = lyric.split(',').map(tag => tag.trim());
 
@@ -25,4 +25,38 @@ async function getSong(lyric){
     }
 }
 
-getSong('dark, dark nights and violent things');
+getSong('dark, dark nights and violent things');*/
+
+let content = document.querySelector(".content");
+
+let prompt = document.querySelector(".prompt");
+
+let input = document.createElement("textarea");
+input.classList.add("input", "text", "prompt");
+
+let lyrics;
+
+/*function whenPromptClicked() {
+    if(input.textContent == undefined){
+        input.textContent = "Type a song lyric!" //maybe rotate through random lyrics
+    }
+}*/
+
+function replacePromptWithInput() {
+    content.appendChild(input);
+    prompt.classList.add("hidden");
+}
+
+prompt.addEventListener("click", () => {
+    replacePromptWithInput();
+    /*whenClickOnInput();
+
+    input.addEventListener("blur", saveText);
+    input.addEventListener("keypress", () => {
+        if (key === "Enter") {
+            saveText();
+        }
+    });
+
+    input.textContent*/
+})
