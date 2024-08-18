30fdc68e30fc8d8749f43630e6f455e09179dedf
idksarah <sarah.p.ngai@gmail.com>
2024-08-18 05:21:32 -0700

    Add Spotify API

--- src/Frontend/script.js
+++ src/Frontend/script.js
@@ -1,27 +1,56 @@
-async function getSong(input) {
-    let url = `http://127.0.0.1:8000/api/find-songs`;
-    console.log(url);
+const spotifyID = '872eb044caa442049aec8c2ffdfb11a7';
+const spotifySecret = 'c7dfaa9ed8bb4eed94c926a1ffdfd60c';
 
+async function getSpotifyToken() {
+    const response = await fetch('https://accounts.spotify.com/api/token', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Authorization': 'Basic ' + btoa(spotifyID + ':' + spotifySecret)
+        },
+        body: 'grant_type=client_credentials'
+    });
+    const data = await response.json();
+    return data.access_token;
+}
+
+async function searchSpotify(query, token) {
+    const response = await fetch(`https://api.spotify.com/v1/search?q=${encodeURIComponent(query)}&type=track&limit=1`, {
+        headers: {
+            'Authorization': 'Bearer ' + token
+        }
+    });
+    const data = await response.json();
+    if (data.tracks.items.length > 0) {
+        const track = data.tracks.items[0];
+        return {
+            name: track.name,
+            artist: track.artists[0].name,
+            album: track.album.name,
+            coverUrl: track.album.images[0].url,
+            songUrl: track.external_urls.spotify
+        };
+    }
+    return null;
+}
+
+async function searchSong(query) {
     try {
-        let response = await fetch(url, {
-            method: "POST",
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(input)
-        });
-        let data = await response.json();
-        console.log(data);
-        return data;
+        const spotifyToken = await getSpotifyToken();
+        const songInfo = await searchSpotify(query, spotifyToken);
+        if (songInfo) {
+            return {
+                ...songInfo,
+            };
+        }
     } catch (error) {
-        console.error(error);
+        console.error('Error:', error);
     }
 }
 
-let test = {
-    "sessionID": "killme",
-    "tags": ["help"]
-};
+searchSong("razzmatazz").then(result => {
+    console.log(result);
+});
 
 //
 
@@ -36,7 +65,7 @@ let outsideInput = true;
 let lyrics;
 
 function whenInputClicked() {
-    if(input.value == "Enter some lyrics!"){
+    if(input.value == "Enter a song/artist name!"){
         input.value = "";
     }
     outsideInput = false;
@@ -50,14 +79,14 @@ function whenInputClicked() {
 function whenEnterOrClickOutside(){
     if(input.value == ""){
         input.classList.remove("yellowText");
-        input.value = "Enter some lyrics!";
+        input.value = "Enter a song/artist name!";
     }
 }
 
 function handleEnter(event){
     if (event.key === "Enter") {
         event.preventDefault();
-        if(input.value == "Enter some lyrics!" || input.value == ""){
+        if(input.value == "Enter a song/artist name!" || input.value == ""){
             input.classList.remove("yellowText");
         } else {
             input.blur();
@@ -70,7 +99,7 @@ function handleEnter(event){
 
 content.appendChild(input);
 input.classList.add("input", "text");
-input.value = "Enter some lyrics!";
+input.value = "Enter a song/artist name!";
 input.addEventListener("click", () => {
     input.classList.add("yellowText");
     whenInputClicked();
@@ -80,7 +109,7 @@ input.addEventListener("mouseleave", () => {
     outsideInput = true;
     document.addEventListener("click", () => {
         if(outsideInput == true){
-            if(input.value != "Enter some lyrics!"){
+            if(input.value != "Enter a song/artist name!"){
                 lyrics = input.value;
                 input.blur();
                 checkIfUserInputText()
@@ -95,63 +124,38 @@ input.addEventListener("keypress", (event) => {
 });
 
 let songContent = document.querySelector(".songContent");
-let song, songArtist, songCover, songLyrics; //assuming only one song is returned
-song = "Don't Like You Anymore";
-songArtist = "Dallon Weekes"
-songLyrics = `[Verse 1]
-Well, I used to have a pretty face
-And it used to be so commonplace
-That I’d always get what I want
-And you’d always get what you want
+let song, songArtist, songCover, songLink, songLyrics, songInfo;
 
-[Chorus]
-And I don’t like you anymore
-And I don’t like you anymore
-But why not?
-But why not?
 
-[Verse 2]
-Oh, don’t you think it's swell
-Oh, you look so fancy but I can tell
-You’re not getting on with your life
-You’re not getting on with your life
-
-[Chorus]
-And I don’t like you anymore
-And I don’t like you anymore
-So just cut yourself with a knife
-Just cut yourself with a knife
-
-[Bridge]
-If you don’t love me anymore
-I think I’ll kill myself
-Because I’m sure as sure can be
-There is nobody else for you but me
-If you don’t love me anymore
-I think I’ll kill myself
-Because I’m sure as sure can be
-There is nobody else for you but me
-
-[Verse 3]
-Well, I used to have a pretty face
-And it used to be so commonplace
-That I’d always get what I want
-And you’d always get what you want`;
+let songContainer = document.createElement("div");
+let streamingContainer = document.createElement("div");
+let DOMsongCover = document.createElement("img");
+let DOMsong = document.createElement("p");
+let DOMsongArtist = document.createElement("p");
+let spotifyLink = document.createElement("a");
 
 function checkIfUserInputText(){
-    if(lyrics != undefined || lyrics != "" || lyrics != "Enter some lyrics!"){
+    if(lyrics != undefined || lyrics != "" || lyrics != "Enter a song/artist name!"){
+        searchSong(lyrics).then(result => { //god damn
+            songInfo = result;
+            DOMsong.textContent = songInfo.name;
+            DOMsongCover.src = songInfo.coverUrl;
+            DOMsongArtist.textContent = songInfo.artist;
+            spotifyLink.href = songInfo.songUrl;
+            spotifyLink.target = "_blank";
+        });
+
+        //
         title.classList.add("hidden");
         input.classList.add("hidden");
 
-        let songContainer = document.createElement("div");
         songContainer.classList.add("songContainer");
         songContent.append(songContainer);
 
-        let streamingContainer = document.createElement("div");
         streamingContainer.classList.add("streamingContainer");
         songContent.append(streamingContainer);
         
-        /*let spotify = document.createElement("img");
+        let spotify = document.createElement("img");
         spotify.src = "https://upload.wikimedia.org/wikipedia/commons/thumb/8/84/Spotify_icon.svg/1982px-Spotify_icon.svg.png";
         spotify.classList.add("spotify", "icon");
         streamingContainer.append(spotify);
@@ -159,26 +163,15 @@ function checkIfUserInputText(){
         let itunes = document.createElement("img");
         itunes.src = "https://static.vecteezy.com/system/resources/previews/046/437/292/non_2x/itunes-music-icon-free-png.png";
         itunes.classList.add("itunes", "icon");
-        streamingContainer.append(itunes);*/
+        streamingContainer.append(itunes);
 
-        let DOMsongCover = document.createElement("img");
-        DOMsongCover.src = "https://images.genius.com/3e7e4a945f18d67189bbb5a603a3e21a.600x600x1.jpg"
         DOMsongCover.classList.add("songCover");
         songContainer.appendChild(DOMsongCover);
 
-        DOMsong = document.createElement("p");
-        DOMsong.textContent = song;
         DOMsong.classList.add("text", "song");
         songContainer.appendChild(DOMsong);
 
-        DOMsongArtist = document.createElement("p");
-        DOMsongArtist.textContent = songArtist;
         DOMsongArtist.classList.add("text", "songArtist");
         songContainer.appendChild(DOMsongArtist);
-
-        DOMsongLyrics = document.createElement("p");
-        DOMsongLyrics.textContent = songLyrics;
-        DOMsongLyrics.classList.add("text", "songLyrics");
-        songContainer.appendChild(DOMsongLyrics);
     }
 }
